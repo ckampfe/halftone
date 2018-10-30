a8b7adcf88f948c800ce2e5a0e119575e0599e7e
Clark Kampfe <clark.kampfe@gmail.com>
2018-10-29 23:43:25 -0500

    extremely crap proof-of-concept halftone overlays at angles

--- src/halftone/app.clj
+++ src/halftone/app.clj
@@ -8,6 +8,13 @@
       (+ 180 adj)
       adj)))
 
+(defn normalize-angle2 [angle]
+  (let [adj (+ 90 (* -1 angle))]
+    adj
+    #_(if (<= adj -90)
+        (+ 180 adj)
+        adj)))
+
 (defn to-radians [degrees]
   (java.lang.Math/toRadians degrees))
 
@@ -36,13 +43,50 @@
              {:x x
               :y y})))
 
+(defn dotvector-seq2 [x y angle dot-distance]
+  (let [normalized-angle (normalize-angle2 angle)
+        {:keys [run rise]} (compute-rise-and-run normalized-angle dot-distance)]
+    (iterate (fn [{:keys [x y]}]
+               {:x (+ x run)
+                :y (+ y rise)})
+             {:x x
+              :y y})))
+
 (defn setup []
   (try
     (q/background 255)
-    (q/fill 100 100 100)
+    (q/no-stroke)
+    #_(q/fill 100 100 100)
 
-    (doseq [{:keys [x y]} (take 50 (dotvector-seq 0 0 60 20))]
-      (q/ellipse x y 5 5))
+    ;; CYAN
+    (q/with-fill [10 120 200 190]
+      (doseq [starter (take 50 (dotvector-seq2 0 0 -30 12))
+              {:keys [x y]} (take 50 (dotvector-seq (:x starter) (:y starter) 60 12))]
+        (q/ellipse x y 5 5))
+
+      (doseq [starter (take 50 (drop 1 (dotvector-seq2 0 0 150 12)))
+              {:keys [x y]} (take 100 (dotvector-seq (:x starter) (:y starter) 60 12))]
+        (q/ellipse x y 5 5)))
+
+    ;; MAGENTA
+    (q/with-fill [200 10 100 190]
+      (doseq [starter (take 50 (dotvector-seq2 0 0 -45 12))
+              {:keys [x y]} (take 100 (dotvector-seq (:x starter) (:y starter) 45 12))]
+        (q/ellipse x y 5 5))
+
+      (doseq [starter (take 50 (drop 1 (dotvector-seq2 0 0 135 12)))
+              {:keys [x y]} (take 100 (dotvector-seq (:x starter) (:y starter) 45 12))]
+        (q/ellipse x y 5 5)))
+
+    ;; YELLOW
+    (q/with-fill [250 200 10 190]
+      (doseq [starter (take 50 (dotvector-seq2 0 0 -15 12))
+              {:keys [x y]} (take 100 (dotvector-seq (:x starter) (:y starter) 75 12))]
+        (q/ellipse x y 5 5))
+
+      (doseq [starter (take 50 (drop 1 (dotvector-seq2 0 0 165 12)))
+              {:keys [x y]} (take 100 (dotvector-seq (:x starter) (:y starter) 75 12))]
+        (q/ellipse x y 5 5)))
 
     (catch Exception e (pr-str (.toString e)))))
 
@@ -50,7 +94,7 @@
        :title "halftone"
        :settings #(q/smooth 2)
        :setup setup
-       :size [500 300])
+       :size [800 500])
      (catch Exception e
        (println e)))
 
