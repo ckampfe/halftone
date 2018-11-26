c0a958d64764cc42e25de2d9c6c13b7dd9e4da1b
Clark Kampfe <clark.kampfe@gmail.com>
2018-11-25 22:31:38 -0600

    comment examples at bottom of matrix.clj

--- src/halftone/matrix.clj
+++ src/halftone/matrix.clj
@@ -56,46 +56,47 @@
                             dot-spacing)]
                (m/array [x y 1])))))
 
-(defn setup []
-  (q/background 255)
-  (q/no-loop))
+(comment
+  (defn setup []
+    (q/background 255)
+    (q/no-loop))
 
-(defn draw []
+  (defn draw []
 
-  (q/no-stroke)
+    (q/no-stroke)
 
-  (let [angle 10 ;; degrees
-        center-x 500
-        center-y 500
-        bounding-box (bounding-box-dots 400 400 500 500 40)
-        rot-in-place (build-rotation center-x center-y (counter-clockwise-rotation angle))
-        inputm (m/array (for [y (range 300 740 40)
-                              x (range 300 740 40)]
-                          [x y 1]))]
+    (let [angle 10 ;; degrees
+          center-x 500
+          center-y 500
+          bounding-box (bounding-box-dots 400 400 500 500 40)
+          rot-in-place (build-rotation center-x center-y (counter-clockwise-rotation angle))
+          inputm (m/array (for [y (range 300 740 40)
+                                x (range 300 740 40)]
+                            [x y 1]))]
 
-    (q/with-fill [0 0 0]
-      (q/text (str angle) 40 40))
+      (q/with-fill [0 0 0]
+        (q/text (str angle) 40 40))
 
-    (q/with-fill [0 200 200]
-      (time (doseq [v (m/mmul bounding-box rot-in-place)]
-              (q/ellipse (m/mget v 0) ;; x
-                         (m/mget v 1) ;; y
-                         10 10))))
+      (q/with-fill [0 200 200]
+        (time (doseq [v (m/mmul bounding-box rot-in-place)]
+                (q/ellipse (m/mget v 0) ;; x
+                           (m/mget v 1) ;; y
+                           10 10))))
 
-    ;; red
-    (q/with-fill [250 180 80]
-      (doseq [[x y] (into [] (map (fn [v] (into [] v))
-                                  inputm))]
-        (q/ellipse x y 10 10)))))
+      ;; red
+      (q/with-fill [250 180 80]
+        (doseq [[x y] (into [] (map (fn [v] (into [] v))
+                                    inputm))]
+          (q/ellipse x y 10 10)))))
 
-(try (q/defsketch example
-       :title "m"
-       :settings #(q/smooth 1)
-       :setup setup
-       :draw draw
-       :size [xdim ydim]
-       ;; :renderer :svg
-       ;; :output-file "out5.svg"
-       :features [:no-bind-output])
-     (catch Exception e
-       (println e)))
+  (try (q/defsketch example
+         :title "m"
+         :settings #(q/smooth 1)
+         :setup setup
+         :draw draw
+         :size [xdim ydim]
+         ;; :renderer :svg
+         ;; :output-file "out5.svg"
+         :features [:no-bind-output])
+       (catch Exception e
+         (println e))))
