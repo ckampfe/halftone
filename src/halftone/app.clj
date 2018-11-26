f5f6026028a06df0eedde7710545a862bc1a3c1d
Clark Kampfe <clark.kampfe@gmail.com>
2018-11-25 22:32:22 -0600

    use matrix math instead of handcoded math for dots

--- src/halftone/app.clj
+++ src/halftone/app.clj
@@ -1,197 +1,135 @@
 (ns halftone.app
-  (:require [quil.core :as q])
+  (:require [halftone.matrix :as hm]
+            [clojure.core.matrix :as m]
+            [quil.core :as q])
   (:import (processing.svg PGraphicsSVG))
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
-(defn normalize-angle [angle]
-  (let [adj (+ 90 (* -1 angle))]
-    (if (<= adj -90)
-      (+ 180 adj)
-      adj)))
+(def width 800)
 
-(defn normalize-angle2 [angle]
-  (+ 90 (* -1 angle)))
-
-(defn to-radians ^double [^double degrees]
-  (java.lang.Math/toRadians degrees))
-
-(defn compute-rise-and-run [angle-b side-a]
-  (let [angle-a 90
-        angle-c (- 180 (+ angle-a angle-b))
-        a-over-sin-side-a (/ side-a (java.lang.Math/sin ^double (to-radians angle-a)))
-        side-b (* (java.lang.Math/sin ^double (to-radians angle-b))
-                  a-over-sin-side-a)
-        side-c (* (java.lang.Math/sin ^double (to-radians angle-c))
-                  a-over-sin-side-a)]
-    {:run side-b
-     :rise side-c}))
-
-(defn dotvector-seq [x y angle dot-distance]
-  (let [normalized-angle (normalize-angle angle)
-        {:keys [run rise]} (compute-rise-and-run normalized-angle dot-distance)]
-    (iterate (fn [{:keys [x y]}]
-               {:x (+ x run)
-                :y (+ y rise)})
-             {:x x
-              :y y})))
-
-(defn dotvector-seq2 [x y angle dot-distance]
-  (let [normalized-angle (normalize-angle2 angle)
-        {:keys [run rise]} (compute-rise-and-run normalized-angle dot-distance)]
-    (iterate (fn [{:keys [x y]}]
-               {:x (+ x run)
-                :y (+ y rise)})
-             {:x x
-              :y y})))
+(def height 500)
 
 (defn sample [x y w h]
   (q/get-pixel x y w h))
 
-(def dot-distance 120)
-(def dot-size 70)
+#_(def dot-distance 120)
+#_(def dot-size 70)
+(def dot-distance 5)
 
-(defn dotseq [rgba angle dot-size dot-distance width height & opts]
-  (let [start-pos -33
-        angle-2 (- angle 90)
-        angle-3 (+ angle 90)
-        boundary-predicate (fn [{:keys [x y]}]
-                             (and (not (> x (+ width dot-distance)))
-                                  (not (> y (+ height dot-distance)))))
+(def dot-size 2)
 
-        ds1 (for [starter (take 100 (dotvector-seq2 (if (> angle-3 180)
-                                                      (+ width start-pos)
-                                                      start-pos)
-                                                    start-pos
-                                                    angle-2
-                                                    dot-distance))
-                  {:keys [x y]} (take-while
-                                 boundary-predicate
-                                 (dotvector-seq (:x starter)
-                                                (:y starter)
-                                                angle
-                                                dot-distance))
-                  :when (and (< x (+ width dot-size))
-                             (> x start-pos)
-                             (< y (+ height dot-size))
-                             (> y start-pos))]
-              {:x x
-               :y y})
-
-        ds2 (for [starter (->> (dotvector-seq2 (if (> angle-3 180)
-                                                 (+ width start-pos)
-                                                 start-pos)
-                                               start-pos
-                                               angle-3
-                                               dot-distance)
-                               (drop 1)
-                               (take 100))
-                  {:keys [x y]} (take-while
-                                 boundary-predicate
-                                 (dotvector-seq (:x starter)
-                                                (:y starter)
-                                                angle
-                                                dot-distance))
-                  :when (and (< x (+ width dot-size))
-                       (> x start-pos)
-                       (< y (+ height dot-size))
-                       (> y start-pos))
-
-                  ]
-                {:x x
-                 :y y})]
-
-    (concat ds1 ds2)))
-
-(defn draw-dots [rgba angle dot-size dot-distance width height & opts]
-  (let [start-pos -33
-        angle-2 (- angle 90)
-        angle-3 (+ angle 90)
-        boundary-predicate (fn [{:keys [x y]}]
-                             (and (not (> x (+ width dot-distance)))
-                                  (not (> y (+ height dot-distance)))))]
+(defn draw-dots! [rgba bounding-box angle width height center-x center-y dot-size]
+  (let [rotation-matrix (hm/build-rotation center-x
+                                           center-y
+                                           (hm/clockwise-rotation angle))]
 
     (q/with-fill rgba
-      (doseq [starter (take 100 (dotvector-seq2 (if (> angle-3 180)
-                                                  (+ width start-pos)
-                                                  start-pos)
-                                                start-pos
-                                                angle-2
-                                                dot-distance))
-              {:keys [x y]} (take-while
-                             boundary-predicate
-                             (dotvector-seq (:x starter)
-                                            (:y starter)
-                                            angle
-                                            dot-distance))]
-        (when (and (< x (+ width dot-size))
-                   (> x start-pos)
-                   (< y (+ height dot-size))
-                   (> y start-pos))
-          (q/ellipse x
-                     y
-                     dot-size
-                     dot-size)))
-
-      (doseq [starter (->> (dotvector-seq2 (if (> angle-3 180)
-                                             (+ width start-pos)
-                                             start-pos)
-                                           start-pos
-                                           angle-3
-                                           dot-distance)
-                           (drop 1)
-                           (take 100))
-              {:keys [x y]} (take-while
-                             boundary-predicate
-                             (dotvector-seq (:x starter)
-                                            (:y starter)
-                                            angle
-                                            dot-distance))]
-        (when (and (< x (+ width dot-size))
-                   (> x start-pos)
-                   (< y (+ height dot-size))
-                   (> y start-pos))
-          (q/ellipse x
-                     y
-                     dot-size
-                     dot-size))))))
+      (doseq [v (m/mmul bounding-box rotation-matrix)
+              :let [x (m/mget v 0)
+                    y (m/mget v 1)]
+              :when (and (< x width)
+                         (> x 0)
+                         (< y height)
+                         (> y 0))]
+        (q/ellipse x y dot-size dot-size)))))
 
 (defn setup []
   (q/background 255)
   (q/no-loop))
 
 (defn draw []
-  (let [cyan (future
-               (q/with-graphics ^PGraphicsSVG (q/create-graphics 800 500 :svg "cyan3.svg")
+  (let [center-x (/ width 2)
+        center-y (/ height 2)
+        bounding-box (hm/bounding-box-dots width
+                                           height
+                                           center-x
+                                           center-y
+                                           dot-distance)
+        cyan (future
+               (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "cyan-mm-small.svg")
                  (q/no-stroke)
-                 (draw-dots [10 120 200 190] 60 dot-size dot-distance 800 500)))
-
-        magenta (future (q/with-graphics ^PGraphicsSVG (q/create-graphics 800 500 :svg "magenta4.svg")
-                          (q/no-stroke)
-                          (let [dots (dotseq [200 10 100 190] 45 dot-size dot-distance 800 500)]
-                            (q/with-fill [200 10 100 190]
-                              (doseq [{:keys [x y]} dots]
-                               (q/ellipse x y dot-size dot-size))))))
-
-        yellow (future (q/with-graphics ^PGraphicsSVG (q/create-graphics 800 500 :svg "yellow3.svg")
-                         (q/no-stroke)
-                         (draw-dots [250 200 10 190] 75 dot-size dot-distance 800 500)))
-
-        black (future (q/with-graphics ^PGraphicsSVG (q/create-graphics 800 500 :svg "black3.svg")
-                        (q/no-stroke)
-                        (draw-dots [0 0 0 45] 110 dot-size dot-distance 800 500)))
-
-        all (future (q/with-graphics ^PGraphicsSVG (q/create-graphics 800 500 :svg "all3.svg")
-                      (q/no-stroke)
-                      ;; cyan
-                      (draw-dots [10 120 200 190] 60 dot-size dot-distance 800 500)
-                      ;; magenta
-                      (draw-dots [200 10 100 190] 45 dot-size dot-distance 800 500)
-                      ;; yellow
-                      (draw-dots [250 200 10 190] 75 dot-size dot-distance 800 500)
-                      ;; black
-                      (draw-dots [0 0 0 45] 110 dot-size dot-distance 800 500)))]
+                 (draw-dots! [10 120 200 190]
+                             bounding-box
+                             60
+                             width
+                             height
+                             center-x
+                             center-y
+                             dot-size)))
+        magenta (future
+                  (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "magenta-mm.svg")
+                    (q/no-stroke)
+                    (draw-dots! [200 10 100 190]
+                                bounding-box
+                                45
+                                width
+                                height
+                                center-x
+                                center-y
+                                dot-size)))
+        yellow (future
+                 (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "yellow-mm.svg")
+                   (q/no-stroke)
+                   (draw-dots! [250 200 10 190]
+                               bounding-box
+                               75
+                               width
+                               height
+                               center-x
+                               center-y
+                               dot-size)))
+        black (future
+                (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "black-mm.svg")
+                  (q/no-stroke)
+                  (draw-dots! [0 0 0 45]
+                              bounding-box
+                              110
+                              width
+                              height
+                              center-x
+                              center-y
+                              dot-size)))
+        all (future
+              (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "all-mm-small.svg")
+                (q/no-stroke)
+                ;; cyan
+                (draw-dots! [10 120 200 190]
+                            bounding-box
+                            60
+                            width
+                            height
+                            center-x
+                            center-y
+                            dot-size)
+                ;; magenta
+                (draw-dots! [200 10 100 190]
+                            bounding-box
+                            45
+                            width
+                            height
+                            center-x
+                            center-y
+                            dot-size)
+                ;; yellow
+                (draw-dots! [250 200 10 190]
+                            bounding-box
+                            75
+                            width
+                            height
+                            center-x
+                            center-y
+                            dot-size)
+                ;; black
+                (draw-dots! [0 0 0 45]
+                            bounding-box
+                            110
+                            width
+                            height
+                            center-x
+                            center-y
+                            dot-size)))]
     (time
      @(future
         @cyan
@@ -206,8 +144,8 @@
        :setup setup
        :draw draw
        :size [800 500]
-       :renderer :svg
-       :output-file "out6.svg"
+       ;; :renderer :svg
+       ;; :output-file "out6.svg"
        :features [:no-bind-output])
      (catch Exception e
        (println e)))
