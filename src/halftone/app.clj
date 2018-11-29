8943ee329596dccbd041c6d7cb57cce45cb28b80
Clark Kampfe <clark.kampfe@gmail.com>
2018-11-29 01:27:15 -0600

    add sampling of input image

--- src/halftone/app.clj
+++ src/halftone/app.clj
@@ -1,5 +1,6 @@
 (ns halftone.app
   (:require [halftone.matrix :as hm]
+            [halftone.color :as c]
             [clojure.core.matrix :as m]
             [quil.core :as q])
   (:import (processing.svg PGraphicsSVG))
@@ -7,9 +8,9 @@
 
 (set! *warn-on-reflection* true)
 
-(def width 800)
+(def width 1280)
 
-(def height 500)
+(def height 720)
 
 (defn sample [x y w h]
   (q/get-pixel x y w h))
@@ -19,21 +20,6 @@
 (def dot-distance 5)
 
 (def dot-size 2)
-
-(defn draw-dots! [rgba bounding-box angle width height center-x center-y dot-size]
-  (let [rotation-matrix (hm/build-rotation center-x
-                                           center-y
-                                           (hm/clockwise-rotation angle))]
-
-    (q/with-fill rgba
-      (doseq [v (m/mmul bounding-box rotation-matrix)
-              :let [x (m/mget v 0)
-                    y (m/mget v 1)]
-              :when (and (< x width)
-                         (> x 0)
-                         (< y height)
-                         (> y 0))]
-        (q/ellipse x y dot-size dot-size)))))
 
 (defn sample-pixels [^processing.core.PImage image x y w h]
   (let [pixels ^ints (q/pixels (q/get-pixel image x y w h))
@@ -47,21 +33,50 @@
                                     (update :r #(+ % (q/red colors)))
                                     (update :g #(+ % (q/green colors)))
                                     (update :b #(+ % (q/blue colors))))))]
-    (-> color-totals
-        (update :r (fn [r] (/ r wxh)))
-        (update :g (fn [g] (/ g wxh)))
-        (update :b (fn [b] (/ b wxh))))))
+    ((juxt :r :g :b)
+     (-> color-totals
+         (update :r (fn [r] (/ r wxh)))
+         (update :g (fn [g] (/ g wxh)))
+         (update :b (fn [b] (/ b wxh)))))))
+
+(def weight-map
+  {:cyan 1.0
+   :magenta 1.0
+   :yellow 1.0
+   :k 1.1})
+
+(defn draw-dots! [rgba bounding-box angle width height center-x center-y dot-size sample-channel weight-map]
+  (let [rotation-matrix (hm/build-rotation center-x
+                                           center-y
+                                           (hm/clockwise-rotation angle))]
+
+    (q/with-fill rgba
+      (doseq [v (m/mmul bounding-box rotation-matrix)
+              :let [x (m/mget v 0)
+                    y (m/mget v 1)
+                    s (sample-pixels (q/state :source-image) x y 2 2)
+                    cmyk (c/rgb->cmyk s)
+                    channel-sample (get cmyk sample-channel)
+                    adjusted-dot-size (* dot-size channel-sample)]
+              :when (and (< channel-sample (get weight-map sample-channel))
+                         (< x width)
+                         (> x 0)
+                         (< y height)
+                         (> y 0))]
+        (q/ellipse x y
+                   dot-size
+                   dot-size
+                   #_adjusted-dot-size
+                   #_adjusted-dot-size)))))
 
 (defn setup []
   (q/background 255)
   (q/no-loop)
   (q/set-state! :source-image
-                (q/load-image "/Users/clark/Downloads/Awww.jpg")))
+                (q/load-image "/Users/clark/Downloads/IMG_2662.jpg")))
 
 (defn draw []
-  (println (sample-pixels (q/state :source-image) 40 40 10 10))
-
-  #_(let [center-x (/ width 2)
+  (let [center-x (/ width 2)
           center-y (/ height 2)
           bounding-box (hm/bounding-box-dots width
                                              height
@@ -78,7 +93,9 @@
                                height
                                center-x
                                center-y
-                               dot-size)))
+                               dot-size
+                               :cyan
+                               weight-map)))
           magenta (future
                     (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "magenta-mm.svg")
                       (q/no-stroke)
@@ -89,7 +106,9 @@
                                   height
                                   center-x
                                   center-y
-                                  dot-size)))
+                                  dot-size
+                                  :magenta
+                                  weight-map)))
           yellow (future
                    (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "yellow-mm.svg")
                      (q/no-stroke)
@@ -100,7 +119,9 @@
                                  height
                                  center-x
                                  center-y
-                                 dot-size)))
+                                 dot-size
+                                 :yellow
+                                 weight-map)))
           black (future
                   (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "black-mm.svg")
                     (q/no-stroke)
@@ -111,7 +132,9 @@
                                 height
                                 center-x
                                 center-y
-                                dot-size)))
+                                dot-size
+                                :k
+                                weight-map)))
           all (future
                 (q/with-graphics ^PGraphicsSVG (q/create-graphics width height :svg "all-mm-small.svg")
                   (q/no-stroke)
@@ -123,7 +146,9 @@
                               height
                               center-x
                               center-y
-                              dot-size)
+                              dot-size
+                              :cyan
+                              weight-map)
                 ;; magenta
                   (draw-dots! [200 10 100 190]
                               bounding-box
@@ -132,7 +157,9 @@
                               height
                               center-x
                               center-y
-                              dot-size)
+                              dot-size
+                              :magenta
+                              weight-map)
                 ;; yellow
                   (draw-dots! [250 200 10 190]
                               bounding-box
@@ -141,7 +168,9 @@
                               height
                               center-x
                               center-y
-                              dot-size)
+                              dot-size
+                              :yellow
+                              weight-map)
                 ;; black
                   (draw-dots! [0 0 0 45]
                               bounding-box
@@ -150,7 +179,10 @@
                               height
                               center-x
                               center-y
-                              dot-size)))]
+                              dot-size
+                              :k
+                              weight-map)))
+        ]
       (time
        @(future
           @cyan
@@ -164,7 +196,7 @@
        :settings #(q/smooth 8)
        :setup setup
        :draw draw
-       :size [800 500]
+       :size [1280 720]
        ;; :renderer :svg
        ;; :output-file "out6.svg"
        :features [:no-bind-output])
